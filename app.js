baba050fe6d951f79b5cf7033cb83893891543b8
Saket kumar <kumarsaket601@gmail.com>
2021-08-03 17:23:10 +0530

    final js

---
+++ app.js
@@ -0,0 +1,46 @@
+var dateOfBirth = document.querySelector("#date-of-birth");
+var luckNumber = document.querySelector("#luck-number");
+var checkButton = document.querySelector("#check-button");
+var output = document.querySelector(".message");
+var log = console.log;
+
+
+function compareSumAndLucky(total,luckNumber){
+
+    if(total%luckNumber === 0){
+
+       output.innerText = "Your Birthday Number is lucky";
+
+    }
+    else{
+        output.innerText = "Sorry your Birthday Number is  not lucky";
+    }
+   
+}
+
+function sumOfBirthday(born){
+
+    var sum = 0;
+
+    for(var postion=0; postion < born.length; postion++){
+          sum = sum + Number(born.charAt(postion));
+
+         
+        
+    }
+    return sum;
+  
+ 
+     
+}
+
+function birthdayButtonHandler() {
+  
+    var born = dateOfBirth.value;
+    var born = born.replaceAll("-","");
+    var total = sumOfBirthday(born);
+    compareSumAndLucky(total,luckNumber.value)
+   
+
+};
+checkButton.addEventListener('click', birthdayButtonHandler)
